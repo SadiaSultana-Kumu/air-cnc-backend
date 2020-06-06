9db85fb07cca480319f08b42820f7d7663467bed
Kumu <kumusadia02@gmail.com>
2020-06-06 12:45:32 +0600

    done

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.get("/homeDetails/:key", (req, res) => {
  
   client = new MongoClient(uri, { useNewUrlParser: true });
   client.connect((error) => {
-    const collection = client.db("air-cnc").collection.find("homeDetails");
+    const collection = client.db("air-cnc").collection("homeDetails");
     collection.find({key}).toArray((err, documents) => {
       if (err) {
         console.log(err);
